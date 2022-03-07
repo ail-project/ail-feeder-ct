203a6f4b7c64e01a1704619c8750808e0ce2e868
David Cruciani <da.cruciani@laposte.net>
2022-03-07 17:38:27 +0100

    chg: [conf] certstream

--- bin/ct.py
+++ bin/ct.py
@@ -18,15 +18,15 @@ else:
     print("[-] No conf file found")
     exit(-1)
 
-if 'cache' in config:
-    cache_expire = config['cache']['expire']
-else:
-    cache_expire = 86400
-
 if 'redis' in config:
     r = redis.Redis(host=config['redis']['host'], port=config['redis']['port'], db=config['redis']['db'])
 else:
     r = redis.Redis(host='localhost', port=6379, db=0)
+
+if 'cerstream' in config:
+    certstreamUrl = config['certstream']['url']
+else:
+    certstreamUrl = 'ws://crd.circl.lu:4000/full-stream'
 
 
 def print_callback(message, context):
@@ -40,15 +40,12 @@ def print_callback(message, context):
 
         if len(all_domains) != 0:
 
-            """sys.stdout.write(u"[{}] {} (SAN: {})\n".format(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'), all_domains[0], ", ".join(all_domains[1:])))
-       
-            r.publish('ct-certs', u"{}\n".format(all_domains))
-        sys.stdout.flush()"""
-
             sys.stdout.write(u"[{}] {} (SAN: {})\n".format(datetime.datetime.now().strftime('%m/%d/%y %H:%M:%S'), all_domains[0], ", ".join(all_domains[1:])))
+
             cert_der = str(message['data']['leaf_cert']['as_der'])
             r.publish('ct-certs', u"{}\n".format(cert_der))
+
             sys.stdout.flush()
         
 
-certstream.listen_for_events(print_callback, url='ws://crd.circl.lu:4000/full-stream')
+certstream.listen_for_events(print_callback, url=certstreamUrl)
