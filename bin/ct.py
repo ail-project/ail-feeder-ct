afd7399fa3f20bc5c0e6fd92133b2477de1fca08
Alexandre Dulaunoy <a@foo.be>
2023-08-04 07:48:35 +0200

    chg: [ct.py] clean-up

--- bin/ct.py
+++ bin/ct.py
@@ -19,17 +19,21 @@ else:
     exit(-1)
 ## Redis Config
 if 'redis' in config:
-    r = redis.Redis(host=config['redis']['host'], port=config['redis']['port'], db=config['redis']['db'])
+    r = redis.Redis(
+        host=config['redis']['host'],
+        port=config['redis']['port'],
+        db=config['redis']['db'],
+    )
 else:
     r = redis.Redis(host='localhost', port=6379, db=0)
-    
+
 ## CertStream URL config
 if len(config['certstream']['url']) >= 1:
     certstreamUrl = config['certstream']['url']
     print("Using Config file CertStream server:")
     print(certstreamUrl)
 else:
-    certstreamUrl = 'wss://certstream.calidog.io/full-stream' # <--This stream is publicly available
+    certstreamUrl = 'wss://certstream.calidog.io/full-stream'  # <--This stream is publicly available
     print("Using fallback CertStream:")
     print(certstreamUrl)
 
@@ -42,10 +46,15 @@ def print_callback(message, context):
     if message['message_type'] == "certificate_update":
         all_domains = message['data']['leaf_cert']['all_domains']
 
-
         if len(all_domains) != 0:
 
-            sys.stdout.write(u"[{}] {} (SAN: {})\n".format(datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'), all_domains[0], ", ".join(all_domains[1:])))
+            sys.stdout.write(
+                u"[{}] {} (SAN: {})\n".format(
+                    datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S'),
+                    all_domains[0],
+                    ", ".join(all_domains[1:]),
+                )
+            )
 
             cert_der = str(message['data']['leaf_cert']['as_der'])
             r.publish('ct-certs', u"{}\n".format(cert_der))
